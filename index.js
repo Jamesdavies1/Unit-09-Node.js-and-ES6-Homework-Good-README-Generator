7b4170b3998febc64ce645c6cbd54350d09798fd
james davies <jamesdavies@jamess-imac.home>
2020-04-24 21:28:08 +0100

    recieved user input as an object in console.log

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
 const inquirer = require("inquirer");
 
-function buildReadme() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "Please begin by adding project title"
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Now include a brief project description"
-    },
-    {
-      type: "input",
-      name: "contents",
-      message: "Add a table of contents"
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Describe the installation process"
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Add the usage for your project"
-    },
-    {
-      type: "input",
-      name: "License",
-      message: "Please include any licenses"
-    },
-    {
-      type: "input",
-      name: "contributors",
-      message: "Name your contributors or leave empty if none"
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "Please include tests"
-    },
-    {
-      type: "input",
-      name: "questions",
-      message: "Finally, add any questions"
-    }
-  ]);
+function questions() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Please begin by adding project title"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Now include a brief project description"
+      },
+      {
+        type: "input",
+        name: "contents",
+        message: "Add a table of contents"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Add the usage for your project"
+      },
+      {
+        type: "input",
+        name: "License",
+        message: "Please include any licenses"
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: "Name your contributors or leave empty if none"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Please include tests"
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Finally, add any questions"
+      }
+    ])
+    .then(userData => {
+      console.log(userData);
+    });
 }
 
-function writeToFile(fileName, data) {}
-
-function init() {
-  buildReadme();
+function createReadMe() {
+  questions();
 }
 
-init();
+createReadMe();
